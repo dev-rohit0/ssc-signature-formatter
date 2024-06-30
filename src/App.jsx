a7398c19fa3ce06ba756508662bfa9bd6decf2d9
dev-rohit0 <rohit.codein@gmail.com>
2024-06-30 20:51:45 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         formData.append('file', file);
 
         try {
-            const response = await fetch('https://sign-formatter-backend.onrender.com/upload', {
+            const response = await fetch('https://sign-formatter-backend.onrender.com/', {
                 method: 'POST',
                 body: formData,
             });
